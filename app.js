8dac5703144663e8051196b199971072a040db1d
mydbol <kobrien@mydbol.com>
2015-12-21 22:06:46 -0500

    almost done

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 // problem : we need a simple way to gwt all the badge earned by a user
 // we will use node.js to connect to the treehoue api and get a user profile info to print out.
-var http=require("https");
+
+var https=require("https");
 var username='kevinobrien2';
 // Connect to the api url 
 
-var printError=functon(error){
-	console.error(requestError.message)
+var printError=function(error){
+		console.error(error.message);
+	
 }
-var request=http.get('https://teamtreehouse.com/'+username+'.json',function(response){
+var request=https.get('https://teamtreehouse.com/'+username+'.json',function(response){
 	console.log(response.statusCode);
 	var body=''
 	response.on('data',function(chunk){
@@ -15,15 +17,23 @@ var request=http.get('https://teamtreehouse.com/'+username+'.json',function(resp
 		body+=chunk;
 	});
 	response.on('end',function(){
-		var profile=JSON.parse(body);
-		printMessage(profile.profile_name,profile.badges.length,profile.points.JavaScript);
+		if(response.statusCode === 200){
+				try{
+					var profile=JSON.parse(body);
+					printMessage(profile.profile_name,profile.badges.length,profile.points.JavaScript);
+				}catch(error){
+					//parse error
+					printError(error); 
+				}
+				
+		}else{
+			printError({message:"There was a problem with your request for the "+username+" profile."}); // send a custom error message
+		}
 	});
 })
 
 
-request.on('error',function(requestError){
-
-});
+request.on('error',printError);
 
 var printMessage=function(userName,badgeCount,Points){
 	var message=userName+' has '+badgeCount+'  badge(s) and '+Points+' points in Javascript!';
